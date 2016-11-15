35aa7ca2442083585383a61da70f0e65e304e2fb
Jason Stephens <videoalchemy@gmail.com>
2016-11-14 21:22:24 -0800

    updated script path for root index. pageg

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var canvas;
+
+
+function setup() {
+  canvas = createCanvas(300,300);
+  canvas.position(window.innerWidth/2,window.innerHeight/2);
+}
+
+function mousePressed(){
+}
+
+function draw() {
+}
