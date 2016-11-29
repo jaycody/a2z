7f870bffa7a3cc72a1fe8f7ba85385a97ee43b96
Jason Stephens <videoalchemy@gmail.com>
2016-11-29 00:46:37 -0800

    using jason.parse for raw data coming from json file

--- wk_04-server_node/07-save_to_json/server.js
+++ wk_04-server_node/07-save_to_json/server.js
@@ -1,3 +1,29 @@
+/* This API shall:
+- PERSISTENCE: save to json: persistance between server instances
+- save to json on change
+- reload json on startup
+- save to temp var words object
+
+STEPS to implement 'save to json' functionality
+1. create the words.json file
+2. in server.js, load from the json file using 'fs' node module (file system API)
+    - fsReadFile vs fsReadFileSync
+3. parse incoming fs info with JSON.parse(data)
+*/
+
+// import from json
+var fs = require('fs');
+var data = fs.readFileSync('words.json');
+var words = JSON.parse(data);
+console.log(words);  // reads the raw data (looks like hex '<Buffer 7b 0a 20 20....')
+
+// hardcoded DB
+var words_array = {
+  'first_word_in_array': 1,
+  'second_word_in_array': 2,
+  'third_word_in_array': 3
+}
+
 var express = require('express');
 var app     = express();
 var server  = app.listen(3000, listening);
@@ -7,13 +33,6 @@ function listening() {
 }
 
 app.use(express.static('public'));
-
-
-var words_array = {
-  'first_word_in_array': 1,
-  'second_word_in_array': 2,
-  'third_word_in_array': 3
-}
 
 
 app.get('/all', showAll);
