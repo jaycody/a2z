d28d51b604221506bf7dd48a5b53b109efac9d9b
Jason Stephens <videoalchemy@gmail.com>
2016-11-29 01:25:47 -0800

    added JSON stringify

--- wk_04-server_node/07-save_to_json/server.js
+++ wk_04-server_node/07-save_to_json/server.js
@@ -9,6 +9,10 @@ STEPS to implement 'save to json' functionality
 2. in server.js, load from the json file using 'fs' node module (file system API)
     - fsReadFile vs fsReadFileSync
 3. parse incoming fs info with JSON.parse(data)
+4. save data anytime a word gets added to the 'words' object
+5. sync vs async AND readFile vs readFileSync
+      sync is a BLOCKING line of code (like when loading data from the server)
+      async uses callbacks while waiting for other connections.  (like when writing info back to the file)
 */
 
 // import from json
@@ -34,17 +38,52 @@ function listening() {
 
 app.use(express.static('public'));
 
+//////////////////////////////////////////
+// JSON db
+app.get('/alljson', showAllJson);
+app.get('/add_to_json/:next_word', addToJson);
 
+// localhost:3000/alljson
+function showAllJson(request, response) {
+  response.send(words);
+}
+
+function addToJson(request, response) {
+  var data = request.params;
+  var word = data.next_word;
+  var reply;
+  words[word] = 10;
+  var wordsToString = JSON.stringify(words, null, 2);
+  fs.writeFile("words.json", wordsToString, finishWrite);  //to file, with info, then callback
+
+  function finishWrite() {
+    console.log('all set');
+    reply = {
+      word: word,
+      score: words[word],
+      status: 'success'
+    }
+    response.send(reply);
+  }
+}
+//////////////////////////////////////////
+
+
+
+//////////////////////////////////////////
+// temp instance context db
 app.get('/all', showAll);
 app.get('/add_to_words_array/:fourth_word', addToWordsArray);
 
+// localhost:3000/all
+function showAll(request, response) {
+  response.send(words_array);
+}
+
+// localhost:3000/add_to_words_array/the_fourth_word
 function addToWordsArray(request, response) {
   var data = request.params;
   var word_to_add = data.fourth_word;
   words_array[word_to_add] = 4;
-  response.send(words_array);
-}
-
-function showAll(request, response) {
   response.send(words_array);
 }
