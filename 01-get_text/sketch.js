eb2817cafb82d4b4f09f34fa169c5a94b5cb932f
Jason Stephens <videoalchemy@gmail.com>
2017-09-06 17:17:17 -0700

    added preload to sequence functions

--- 01-get_text/sketch.js
+++ 01-get_text/sketch.js
@@ -1,8 +1,20 @@
 // landing page index
-//    lists links to exercises from a text file 
+//    lists links to exercises from a text file
 
+
+var exercises;
+
+function preload() {
+  exercises = loadStrings("exercise-list.txt", readExercises);
+}
 
 function setup() {
+  noCanvas();
 
+
+}
+
+function readExercises(exercises) {
+  console.log(exercises);
 
 }
