0bbf8b7230b6012d92b8aa73c42805c96b123095
Jason Stephens <videoalchemy@gmail.com>
2017-09-08 19:42:02 -0700

    auto generate linked index for 01-get_text section

--- 01-get_text/sketch.js
+++ 01-get_text/sketch.js
@@ -3,6 +3,10 @@
 
 
 var exercises;
+var link1;
+var sectionTitle;
+
+var links = []; // array to hold all exercises
 
 function preload() {
   exercises = loadStrings("exercise-list.txt", readExercises);
@@ -11,10 +15,24 @@ function preload() {
 function setup() {
   noCanvas();
 
+  /////
+  // create section header from 1 element in the exercises list
+  sectionTitle = createElement('h2', exercises[0]);
+
+
+  //////
+  // create and display p element for each exercise in the exercise list
+  for (var i = 1; i < exercises.length; i++) {
+      links[i] = createP("<a href='" + exercises[i] + "'>" + exercises[i] + "</a>");
+    }
+}
+
+function draw() {
 
 }
 
 function readExercises(exercises) {
   console.log(exercises);
+
 
 }
